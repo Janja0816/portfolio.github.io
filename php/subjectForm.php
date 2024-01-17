19dcbbe06d98090cf431a7b2ea7575a36449f1f9
Janja0816 <98919639+Janja0816@users.noreply.github.com>
2024-01-17 21:43:01 +0100

    Add files via upload

--- php/subjectForm.php
+++ php/subjectForm.php
@@ -1,16 +1,4 @@
 <?php
-    // if($_SERVER["REQUEST_METHOD"] == "POST"){
-    //     $name = $_POST["name"];
-    //     $email = $_POST["email"];
-    //     $message = $_POST["message"]
-
-
-    //     $to = "nikolajanjicrs@gmial.com";
-    //     $subject = $_POST["subject"];
-    //     $headers = "From: $email";
-
-    //     mail($to,$subject,$message,$headers);
-    // }
 
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
@@ -21,38 +9,58 @@
     require 'phpmailer/src/SMTP.php';
 
     if(isset($_POST["send"])){
-        // $name = $_POST["name"];
-        // $email = $_POST["email"];
-        // $message = $_POST["message"]
+        
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $errors = array();
 
-        $mail = new PHPMailer(true);
+        $checkName = '/^[A-ZČĆŽŠĐ][a-zčćžšđ]{2,10}$/';
+        $checkEmail = '/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/';
+        $checkMessage = '/[A-z]{3,}/';
 
-        $mail->IsSMTP;
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'nikolajanjicrs@gmail.com';
-        $mail->Password = '';
-        $mail->SMTPSecure='ssl';
-        $mail->Port = 465;
+        if (!preg_match($checkName, $name)) {
+            $errors[] = 'The name is not correctly written.';
+        }
 
-        $mail->setFrom('nikolajanjicrs@gmail.com');
+        if (!preg_match($checkEmail, $email)) {
+            $errors[] = 'Please enter the correct email.';
+        }
 
-        $mail->addAddress($_POST["email"]);
+        if (!preg_match($checkMessage, $message)) {
+            $errors[] = 'Write a message.';
+        }
 
-        $mail->isHTML(true);
-
-        $mail->Subject = $_POST["subject"];
-        $mail->Body = $_POST["message"];
-
-        $mail->send();
-
-        echo
-        "
-        <script>
-        alert('Sent Successfully');
-        document.location.href='index.html'
-        </script>
-        "
+        if (!empty($errors)) {
+            echo json_encode($errors);
+        } else {
+            $mail = new PHPMailer(true);
+    
+            $mail->IsSMTP;
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'nikolajanjicrs@gmail.com';
+            $mail->Password = '';
+            $mail->SMTPSecure='ssl';
+            $mail->Port = 465;
+    
+            $mail->setFrom('nikolajanjicrs@gmail.com');
+    
+            $mail->addAddress($email);
+    
+            $mail->isHTML(true);
+    
+            $mail->Subject = $_POST["subject"];
+            $mail->Body = $name. " " .$message;
+    
+            try {
+                $mail->send();
+                echo json_encode(array('success' => 'You have successfully sent the email.'));
+            } catch (Exception $e) {
+                echo json_encode(array('error' => 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo));
+            }
+    
+        }
 
 
     }
